ba3079fca03256d14b70fad99677b27091e0a489
edusatelis <edusatelis@gmail.com>
2021-01-06 22:41:12 -0300

    criando repositorio para manipular o endpoint user

---
+++ src/users/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+  BaseEntity,
+  Entity,
+  Unique,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@Unique(['email'])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 200 })
+  email: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 200 })
+  name: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 20 })
+  role: string;
+
+  @Column({ nullable: false, default: true })
+  status: boolean;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
